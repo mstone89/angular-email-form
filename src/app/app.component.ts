a110774b5880aaebe709580204c6ba00922fd56a
Molly Stone <molly.stone89@gmail.com>
2020-02-22 12:20:16 -0500

    Create email validation using angular template forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'emailform';
+    email: string = '';
+    submittedMessage: string;
+
+    constructor() {
+
+    }
+
+    onSubmit() {
+        this.submittedMessage = 'Email submitted: ' + this.email;
+    }
 }
